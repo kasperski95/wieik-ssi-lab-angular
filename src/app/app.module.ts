fc182df49ec444bf6dbbaf2f3350811c39a2579c
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2020-12-11 18:15:49 +0100

    clean project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { SummaryComponent } from './components/summary/summary.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ManageComponent } from './components/manage/manage.component';
 import { FormsModule } from '@angular/forms';
-import { AppRoutes } from './app.routes';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SummaryComponent,
-    DashboardComponent,
-    ManageComponent,
-  ],
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutes],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
