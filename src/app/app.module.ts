6e2792d28f4c25404897113c910532e89a8c8255
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2020-12-16 17:01:34 +0100

    reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MetricComponent } from './components/metric/metric.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { NodesRowComponent } from './components/nodes-row/nodes-row.component';
-import { NodesComponent } from './components/nodes/nodes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    DashboardComponent,
-    MetricComponent,
-    NodesComponent,
-    NodesRowComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule, NgbProgressbarModule],
   providers: [],
   bootstrap: [AppComponent],
