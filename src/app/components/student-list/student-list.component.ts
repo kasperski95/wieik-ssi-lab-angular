2ae8f20f7ec7d920f8b9d65bbbfd1d1953278b2a
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2020-12-16 18:08:52 +0100

    display active student

--- src/app/components/student-list/student-list.component.ts
+++ src/app/components/student-list/student-list.component.ts
@@ -9,9 +9,28 @@ import { StudentsRepositoryService } from 'src/app/services/students-repository.
 })
 export class StudentListComponent implements OnInit {
   students: Student[];
+  averageGrade: number;
+  activeStudent?: Student;
 
   constructor(private studentRepository: StudentsRepositoryService) {
     this.students = studentRepository.findAll();
+  }
+
+  setActiveStudent(student: Student) {
+    if (student === this.activeStudent) {
+      this.activeStudent = undefined;
+    } else {
+      this.activeStudent = student;
+      this.averageGrade = this.calculateAverageGrade(this.activeStudent);
+    }
+  }
+
+  private calculateAverageGrade(student: Student) {
+    const grades = student.oceny.filter((ocena) => !!ocena.wartosc);
+    return (
+      grades.reduce((acc, { wartosc }) => acc + wartosc, 0) /
+      (grades.length || 1)
+    );
   }
 
   ngOnInit(): void {}
